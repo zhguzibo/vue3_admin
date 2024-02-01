b0393051efdfb4f9fb25b5ef8fc563ccd8f00196
zhguzibo <zhguzibo@163.com>
2024-02-01 18:06:29 +0800

    仓库创建

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+const str = '哈哈哈'
+</script>
+
+<template>
+  <div class="box">
+    <h1>App</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box{
+  width: 600px;
+  height: 400px;
+  background-color: aliceblue;
+  h1{
+    color: black;
+  }
+}
+</style>
